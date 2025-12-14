434178b464d7d132db67cc869dbbd062e0940922
kenro-t <ngkn1425@gmail.com>
2025-12-14 22:40:00 +0900

    デフォルトリージョン追加

--- bin/cdk-pipelines-playground.ts
+++ bin/cdk-pipelines-playground.ts
@@ -3,4 +3,9 @@ import * as cdk from "aws-cdk-lib";
 import { CdkPipelinesPlaygroundStack } from "../lib/cdk-pipelines-playground-stack";
 
 const app = new cdk.App();
-new CdkPipelinesPlaygroundStack(app, "CdkPipelinesPlaygroundStack", {});
+new CdkPipelinesPlaygroundStack(app, "CdkPipelinesPlayground", {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+});
