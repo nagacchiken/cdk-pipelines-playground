8d372015d8421fcec50545a89d7686d500b2f69a
kenro-t <ngkn1425@gmail.com>
2025-12-15 23:59:12 +0900

    update

--- bin/cdk-pipelines-playground.ts
+++ bin/cdk-pipelines-playground.ts
@@ -1,11 +1,6 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
-import { CdkPipelinesPlaygroundStack } from "../lib/cdk-pipelines-playground-stack";
+import { CdkPipelinesPlaygroundV2Stack } from "../lib/cdk-pipelines-playground-stack";
 
 const app = new cdk.App();
-new CdkPipelinesPlaygroundStack(app, "CdkPipelinesPlayground", {
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION,
-  },
-});
+new CdkPipelinesPlaygroundV2Stack(app, "CdkPipelinesPlaygroundV2");
--- lib/cdk-pipelines-playground-stack.ts
+++ lib/cdk-pipelines-playground-stack.ts
@@ -8,15 +8,21 @@ import {
 import * as sm from "aws-cdk-lib/aws-secretsmanager";
 import { MyAppStage } from "./my-app-stage";
 
-export class CdkPipelinesPlaygroundStack extends cdk.Stack {
+export class CdkPipelinesPlaygroundV2Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const githubToken = sm.Secret.fromSecretNameV2(
-      this,
-      "GitHubToken",
-      "cdk-pipelines-playground-token"
-    );
+    // const githubToken = sm.Secret.fromSecretNameV2(
+    //   this,
+    //   "GitHubToken",
+    //   "cdk-pipelines-playground-token"
+    // );
+
+    const githubToken = sm.Secret.fromSecretAttributes(this, "GitHubToken", {
+      secretCompleteArn:
+        "arn:aws:secretsmanager:ap-northeast-1:myarn",
+    });
+
     const repo = "nagacchiken/cdk-pipelines-playground";
     const branch = "main";
 
@@ -24,9 +30,7 @@ export class CdkPipelinesPlaygroundStack extends cdk.Stack {
       pipelineName: "cdk-pipelines-playground-pipeline",
       synth: new ShellStep("Synth", {
         input: CodePipelineSource.gitHub(repo, branch, {
-          authentication: githubToken.secretValueFromJson(
-            "cdk-pipelines-playground-token"
-          ),
+          authentication: githubToken.secretValueFromJson("github-token"),
         }),
         commands: ["npm ci", "npm run build", "npx cdk synth"],
       }),
@@ -36,3 +40,34 @@ export class CdkPipelinesPlaygroundStack extends cdk.Stack {
     const deployStage = pipeline.addStage(deploy);
   }
 }
+
+// import * as cdk from "aws-cdk-lib";
+// import { Construct } from "constructs";
+// import {
+//   CodePipeline,
+//   CodePipelineSource,
+//   ShellStep,
+// } from "aws-cdk-lib/pipelines";
+// import { MyAppStage } from "./my-app-stage";
+
+// export class CdkPipelinesPlaygroundStack extends cdk.Stack {
+//   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+//     super(scope, id, props);
+
+//     const repo = "nagacchiken/cdk-pipelines-playground";
+//     const branch = "main";
+
+//     const pipeline = new CodePipeline(this, "Pipeline", {
+//       pipelineName: "cdk-pipelines-playground-pipeline",
+//       synth: new ShellStep("Synth", {
+//         input: CodePipelineSource.connection(repo, branch, {
+//           connectionArn: "arn:aws:codeconnections:myarn",
+//         }),
+//         commands: ["npm ci", "npm run build", "npx cdk synth"],
+//       }),
+//     });
+
+//     const deploy = new MyAppStage(this, "Deploy");
+//     const deployStage = pipeline.addStage(deploy);
+//   }
+// }
